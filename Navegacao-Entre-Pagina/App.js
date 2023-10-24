cf8f2a67899d1eaeac872b4e191ea7b17ed54f05
Kaue-cc <kauecoficial@gmail.com>
2023-10-24 19:31:04 -0300

    sa

--- Navegacao-Entre-Pagina/App.js
+++ Navegacao-Entre-Pagina/App.js
@@ -3,7 +3,7 @@ import { useEffect, useState, useMemo } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from  "@react-navigation/native-stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Pagina1 from './src/Pagina1';
 import Pagina2 from './src/Pagina2';
@@ -14,11 +14,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={({ navigation}) => 
-        <View>
-          <Text>Home</Text>
-          <Button title='1' onPress={() => navigation.navigate("Pagina1")}></Button>
-          <Button title='2' onPress={() => navigation.navigate("Pagina2")}></Button>
+        <Stack.Screen name="Home" component={({ navigation }) =>
+          <View>
+            <Text>Home</Text>
+            <Button title='1' onPress={() => navigation.navigate("Pagina1")}></Button>
+            <Button title='2' onPress={() => navigation.navigate("Pagina2")}></Button>
           </View>} />
         <Stack.Screen name="Pagina1" component={Pagina1} />
         <Stack.Screen name="Pagina2" component={Pagina2} />
