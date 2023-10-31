b1bdeea4108facb7a7be52dc3d0eb2e226bef458
Kaue-cc <kauecoficial@gmail.com>
2023-10-31 20:38:47 -0300

    Documentação feita

--- Atividades/Aplicativo-de-Lista/src/HomeScreen/HomeScreen.js
+++ Atividades/Aplicativo-de-Lista/src/HomeScreen/HomeScreen.js
@@ -3,69 +3,77 @@ import { View, Text, Button, TouchableOpacity, TextInput } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function HomeScreen({ navigation }) {
-  const [lists, setLists] = useState([]);
-  const [listToEdit, setListToEdit] = useState(null); // Para rastrear a lista sendo editada
-  const [editedListName, setEditedListName] = useState(""); // Para rastrear o nome editado
+  // Definindo estados com hooks do React.
+  const [lists, setLists] = useState([]); // Armazena a lista de objetos de lista.
+  const [listToEdit, setListToEdit] = useState(null); // Rastreia a lista que está sendo editada.
+  const [editedListName, setEditedListName] = useState(""); // Rastreia o nome editado da lista.
 
+  // UseEffect é utilizado para carregar as listas salvas no AsyncStorage ao iniciar a tela.
   useEffect(() => {
     async function loadLists() {
       try {
-        const savedLists = await AsyncStorage.getItem("lists");
+        const savedLists = await AsyncStorage.getItem("lists"); // Lê as listas salvas no AsyncStorage.
         if (savedLists) {
-          const parsedLists = JSON.parse(savedLists);
-          setLists(parsedLists);
+          const parsedLists = JSON.parse(savedLists); // Analisa as listas em formato JSON.
+          setLists(parsedLists); // Atualiza o estado com as listas recuperadas.
         }
       } catch (error) {
         console.error("Error loading lists: ", error);
       }
     }
 
-    loadLists();
+    loadLists(); // Chama a função para carregar as listas ao montar a tela.
   }, []);
 
+  // Função para adicionar uma nova lista à tela HomeScreen.
   const addList = (newListName) => {
     if (newListName) {
-      const newList = { id: Date.now(), name: newListName };
-      const updatedLists = [...lists, newList];
-      setLists(updatedLists);
-      saveListsToStorage(updatedLists);
+      const newList = { id: Date.now(), name: newListName }; // Cria um objeto de lista com um ID único.
+      const updatedLists = [...lists, newList]; // Adiciona a nova lista à lista existente.
+      setLists(updatedLists); // Atualiza o estado com a nova lista.
+      saveListsToStorage(updatedLists); // Salva as listas atualizadas no AsyncStorage.
     }
   };
 
+  // Função para editar uma lista.
   const editList = (id) => {
-    setListToEdit(id);
-    const listToEdit = lists.find((list) => list.id === id);
+    setListToEdit(id); // Define a lista que está sendo editada.
+    const listToEdit = lists.find((list) => list.id === id); // Encontra a lista pelo ID.
     if (listToEdit) {
-      setEditedListName(listToEdit.name);
+      setEditedListName(listToEdit.name); // Define o nome da lista a ser editada.
     }
   };
 
+  // Função para salvar as alterações feitas na lista editada.
   const saveEditedList = () => {
     if (listToEdit !== null && editedListName.trim() !== "") {
       const updatedLists = lists.map((list) =>
         list.id === listToEdit ? { ...list, name: editedListName } : list
-      );
-      setLists(updatedLists);
-      saveListsToStorage(updatedLists);
-      setListToEdit(null);
-      setEditedListName("");
+      ); // Mapeia as listas e atualiza a lista editada.
+      setLists(updatedLists); // Atualiza o estado com as listas editadas.
+      saveListsToStorage(updatedLists); // Salva as listas atualizadas no AsyncStorage.
+      setListToEdit(null); // Limpa a lista sendo editada.
+      setEditedListName(""); // Limpa o nome editado da lista.
     }
   };
 
+  // Função para excluir uma lista.
   const deleteList = (id) => {
-    const updatedLists = lists.filter((list) => list.id !== id);
-    setLists(updatedLists);
-    saveListsToStorage(updatedLists);
+    const updatedLists = lists.filter((list) => list.id !== id); // Filtra a lista a ser excluída.
+    setLists(updatedLists); // Atualiza o estado com a lista excluída.
+    saveListsToStorage(updatedLists); // Salva as listas atualizadas no AsyncStorage.
   };
 
+  // Função para salvar as listas no AsyncStorage.
   const saveListsToStorage = async (listsToSave) => {
     try {
-      await AsyncStorage.setItem("lists", JSON.stringify(listsToSave));
+      await AsyncStorage.setItem("lists", JSON.stringify(listsToSave)); // Converte e salva as listas em formato JSON.
     } catch (error) {
       console.error("Error saving lists: ", error);
     }
   };
 
+  // Renderiza a interface da tela HomeScreen.
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
@@ -73,7 +81,7 @@ function HomeScreen({ navigation }) {
         title="Add List"
         onPress={() => {
           navigation.navigate("ListaScreen", {
-            onAddList: addList,
+            onAddList: addList, // Passa a função para adicionar lista à tela de adição de lista.
           });
         }}
       />
--- Atividades/Aplicativo-de-Lista/src/ListaScreen/ListaScreen.js
+++ Atividades/Aplicativo-de-Lista/src/ListaScreen/ListaScreen.js
@@ -1,30 +1,45 @@
 // ListaScreen.js
+
 import React, { useState } from "react";
 import { View, Text, Button, TextInput } from "react-native";
 
+// Componente ListaScreen
 function ListaScreen({ route, navigation }) {
+  // Estado local para rastrear o nome da nova lista
   const [newListName, setNewListName] = useState("");
 
+  // Extrair a função onAddList dos parâmetros da rota
   const { onAddList } = route.params;
 
+  // Função chamada ao adicionar uma nova lista
   const addList = () => {
+    // Verifica se o nome da nova lista não está vazio
     if (newListName) {
+      // Chama a função onAddList passando o nome da nova lista
       onAddList(newListName);
+
+      // Navega de volta para a tela anterior (HomeScreen)
       navigation.goBack();
     }
   };
 
+  // Renderização da tela
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Lista Screen</Text>
+
+      {/* Input de texto para inserir o nome da nova lista */}
       <TextInput
         placeholder="Nome da Lista"
         value={newListName}
         onChangeText={(text) => setNewListName(text)}
       />
+
+      {/* Botão para adicionar a nova lista */}
       <Button title="Adicionar Lista" onPress={addList} />
     </View>
   );
 }
 
+// Exporta o componente ListaScreen para ser usado em outras partes do aplicativo
 export default ListaScreen;
